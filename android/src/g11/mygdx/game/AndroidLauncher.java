02fedb7608419a48f987688dcbc4b6ce784fbaff
Oddaspa <odd.gunnar.aspaas@gmail.com>
2019-03-19 11:15:47 +0100

    First commit

---
+++ android/src/g11/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package g11.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import g11.mygdx.game.BattleSheep;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BattleSheep(), config);
+	}
+}
