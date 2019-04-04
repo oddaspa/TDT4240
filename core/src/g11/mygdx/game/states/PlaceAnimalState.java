e2bb112317146d96dc5335dcbbfa21fe27aa7058
Oddaspa <odd.gunnar.aspaas@gmail.com>
2019-04-04 14:54:50 +0200

    Merger leftovers

--- core/src/g11/mygdx/game/states/PlaceAnimalState.java
+++ core/src/g11/mygdx/game/states/PlaceAnimalState.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.util.Random;
 
